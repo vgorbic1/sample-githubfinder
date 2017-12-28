1b299708aa3a0068c08991d681969868d989912c
Vlad Gorbich <vladgorbich@gmail.com>
2017-12-28 17:44:56 -0600

    final commit

---
+++ githubfinder/app.js
@@ -0,0 +1,23 @@
+// Init GitHub
+const github = new GitHub;
+const ui = new UI;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (e) => {
+  const userText = e.target.value;
+  if (userText !== '') {
+    github.getUser(userText)
+      .then(data => {
+        if (data.profile.message === 'Not Found') {
+          ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      });
+    } else {
+        ui.clearProfile();
+    }
+})
---
+++ githubfinder/github.js
@@ -0,0 +1,23 @@
+class GitHub {
+  constructor() {
+    this.client_id = '80d002d8b26b5b2c2ce3';
+    this.client_secret = '9c8eaeae641e5be6027b27fadb893d21580898a5';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+
+  }
+}
